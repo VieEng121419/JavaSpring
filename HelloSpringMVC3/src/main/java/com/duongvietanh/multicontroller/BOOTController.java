706428d8a0b348a81fbfaa0560e82c56a09b5d00
VieEng121419 <vietanh1419@gmail.com>
2020-10-03 17:59:36 +0700

    HelloSpringMVC3

---
+++ HelloSpringMVC3/src/main/java/com/duongvietanh/multicontroller/BOOTController.java
@@ -0,0 +1,15 @@
+package com.duongvietanh.multicontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BOOTController {
+	@RequestMapping("/boot")
+	public ModelAndView showMVC()
+	{
+		ModelAndView mv = new ModelAndView("view_boot");
+		return mv;
+	}
+}
